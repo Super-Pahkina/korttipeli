1681b6465ed84fc5e94d15e54c946eaf83bfc671
Kristiina Kumila <kristiinakumila@gmail.com>
2021-02-01 12:35:40 +0200

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Buttoncoponent from './components/Buttoncoponent'
 
 export default function App() {
+  const [data, setData] = useState([])
+
+  useEffect( ()=> fetchData())
+
+  const fetchData =()=> {
+    fetch('https://fineli.fi/fineli/api/v1/foods?q=33128')
+    .then(response => response.json())
+    .then(json => setData(JSON.stringify(json)))
+    .catch((error) => console.error(error))
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{marginTop:100}}>HIENO ÄPPI</Text>
+      <Text>{data}</Text>
+      <Buttoncoponent></Buttoncoponent>
       <StatusBar style="auto" />
     </View>
   );
